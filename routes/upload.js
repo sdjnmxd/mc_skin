534f296dba0f9f57e497ff4f062987484c115bc5
MXD <i@mxd.moe>
2015-10-19 23:58:11 +0800

    记录下上传日志,便于生产环境调试

--- routes/upload.js
+++ routes/upload.js
@@ -8,6 +8,7 @@
 var express = require('express');
 var router = express.Router();
 var session = require('../modules/session');
+var userlog = require('../modules/userlog');
 
 router.get('/', function (req, res, next) {
     res.status(405);
@@ -23,18 +24,21 @@ router.post('/', function (req, res, next) {
         var fileName = file.name;
 
         if (fileSize > 50) {
+            userlog.consoleLog(req, '[upload]上传文件失败,状态码：413 - 文件过大');
             res.status(413);
             res.json({
                 statusCode: 413,
                 msg: '文件过大,要求小于等于50KB'
             })
         } else if (fileExtension != 'png') {
+            userlog.consoleLog(req, '[upload]上传文件失败,状态码：415 - 文件格式不正确');
             res.status(415);
             res.json({
-                statusCode: 413,
+                statusCode: 415,
                 msg: '文件格式不正确,要求为PNG格式'
             });
         } else if (!session.checkUserSession(req)) {
+            userlog.consoleLog(req, '[upload]上传文件失败,状态码：401 - accessToken异常');
             res.status(401);
             res.json({
                 statusCode: 401,
@@ -43,12 +47,14 @@ router.post('/', function (req, res, next) {
         } else {
             var text = '文件大小：' + fileSize + 'kb\n' + '文件类型：' + fileExtension + '\n' + '文件名称：' + fileName;
             //console.log(text);
+            userlog.consoleLog(req, '[upload]上传文件成功,状态码：200 - AllClear');
             res.json({
                 statusCode: 200,
                 msg: '上传成功！'
             });
         }
     } else {
+        userlog.consoleLog(req, '[upload]上传文件失败,状态码：400 - file为空');
         res.status(400);
         res.json({
             statusCode: 400,
