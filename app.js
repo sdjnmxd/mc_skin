203961b75800b8f653a6be7adfa7704dae99f682
MXD <i@mxd.moe>
2015-11-02 13:10:10 +0800

    丢掉favicon

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -26,9 +25,6 @@ app.set('view engine', 'ejs');
 if (config.debug_log.enable) {
     app.use(logger('combined'));
 }
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 //bodyParser
 app.use(bodyParser.json());
